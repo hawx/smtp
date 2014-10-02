3dddd6fd8d954eedb23009240969dd233d5cf3c9
Joshua Hawxwell <m@hawx.me>
2014-10-02 16:50:00 +0100

    Add test sending to multiple recipients

--- server_test.go
+++ server_test.go
@@ -61,6 +61,46 @@ func TestSenderRecipientBodyAndQuit(t *testing.T) {
 	})
 }
 
+func TestMessageToMultipleRecipients(t *testing.T) {
+	assert := assert.New(t)
+
+	with(t, func(s Server) {
+		c, err := smtp.Dial(ADDR)
+		assert.Nil(err)
+
+		assert.Nil(c.Mail("sender@example.org"))
+		assert.Nil(c.Rcpt("recipient1@example.net"))
+		assert.Nil(c.Rcpt("recipient2@example.net"))
+		assert.Nil(c.Rcpt("recipient3@example.net"))
+
+		wc, err := c.Data()
+		assert.Nil(err)
+		_, err = fmt.Fprintf(wc, "This is the email body")
+		assert.Nil(err)
+		assert.Nil(wc.Close())
+
+		called := make(chan struct{})
+		s.Handle(func(msg Message) {
+			assert.Equal("sender@example.org", msg.Sender)
+			assert.Equal(3, len(msg.Recipients))
+			assert.Equal("recipient1@example.net", msg.Recipients[0])
+			assert.Equal("recipient2@example.net", msg.Recipients[1])
+			assert.Equal("recipient3@example.net", msg.Recipients[2])
+			assert.Equal("This is the email body\n", msg.Data)
+			close(called)
+		})
+
+		assert.Nil(c.Quit())
+
+		select {
+		case <-called:
+		case <-time.After(time.Second):
+			t.Log("timed out")
+			t.Fail()
+		}
+	})
+}
+
 func TestSenderRecipientBodyAndQuitWithReset(t *testing.T) {
 	assert := assert.New(t)
 
