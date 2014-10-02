c00fe14e0b2535c7dde2eb1d60a7ef3c481266d7
Joshua Hawxwell <m@hawx.me>
2014-10-02 16:21:01 +0100

    Delete pointless tests

--- server_test.go
+++ server_test.go
@@ -25,56 +25,6 @@ func with(t *testing.T, f func(Server)) {
 	s.Close()
 }
 
-func TestConnect(t *testing.T) {
-	assert := assert.New(t)
-
-	with(t, func(s Server) {
-		_, err := smtp.Dial(ADDR)
-		assert.Nil(err)
-	})
-}
-
-func TestSender(t *testing.T) {
-	assert := assert.New(t)
-
-	with(t, func(s Server) {
-		c, err := smtp.Dial(ADDR)
-		assert.Nil(err)
-
-		assert.Nil(c.Mail("sender@example.org"))
-	})
-}
-
-func TestSenderAndRecipient(t *testing.T) {
-	assert := assert.New(t)
-
-	with(t, func(s Server) {
-		c, err := smtp.Dial(ADDR)
-		assert.Nil(err)
-
-		assert.Nil(c.Mail("sender@example.org"))
-		assert.Nil(c.Rcpt("recipient@example.net"))
-	})
-}
-
-func TestSenderRecipientAndBody(t *testing.T) {
-	assert := assert.New(t)
-
-	with(t, func(s Server) {
-		c, err := smtp.Dial(ADDR)
-		assert.Nil(err)
-
-		assert.Nil(c.Mail("sender@example.org"))
-		assert.Nil(c.Rcpt("recipient@example.net"))
-
-		wc, err := c.Data()
-		assert.Nil(err)
-		_, err = fmt.Fprintf(wc, "This is the email body")
-		assert.Nil(err)
-		assert.Nil(wc.Close())
-	})
-}
-
 func TestSenderRecipientBodyAndQuit(t *testing.T) {
 	assert := assert.New(t)
 
