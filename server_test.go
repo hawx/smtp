a02afc5affca11c63d42e37428dc7ed0283025cf
Joshua Hawxwell <m@hawx.me>
2014-10-15 15:35:38 +0100

    remove with function in tests

--- server_test.go
+++ server_test.go
@@ -85,253 +85,249 @@ func NewCatchServer(t *testing.T) (Server, <-chan Message) {
 	return s, ch
 }
 
-func with(t *testing.T, f func(Server)) {
-	s, err := Listen(ADDR, NAME)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	time.Sleep(time.Millisecond)
-	f(s)
-
-	s.Close()
-}
-
 func TestSenderRecipientBodyAndQuit(t *testing.T) {
 	assert := assert.New(t)
 
-	with(t, func(s Server) {
-		called := make(chan struct{})
-		s.Handle(func(msg Message) {
-			assert.Equal("sender@example.org", msg.Sender)
-			if assert.Equal(1, len(msg.Recipients)) {
-				assert.Equal("recipient@example.net", msg.Recipients[0])
-			}
-			assert.Equal("This is the email body\n", msg.Data)
-			close(called)
-		})
+	s := NewServer(t)
+	defer s.Close()
 
-		c, err := smtp.Dial(ADDR)
-		assert.Nil(err)
-
-		assert.Nil(c.Mail("sender@example.org"))
-		assert.Nil(c.Rcpt("recipient@example.net"))
-
-		wc, err := c.Data()
-		assert.Nil(err)
-		_, err = fmt.Fprintf(wc, "This is the email body")
-		assert.Nil(err)
-		assert.Nil(wc.Close())
-
-		assert.Nil(c.Quit())
-
-		select {
-		case <-called:
-		case <-time.After(time.Second):
-			t.Log("timed out")
-			t.Fail()
+	called := make(chan struct{})
+	s.Handle(func(msg Message) {
+		assert.Equal("sender@example.org", msg.Sender)
+		if assert.Equal(1, len(msg.Recipients)) {
+			assert.Equal("recipient@example.net", msg.Recipients[0])
 		}
+		assert.Equal("This is the email body\n", msg.Data)
+		close(called)
 	})
+
+	c, err := smtp.Dial(ADDR)
+	assert.Nil(err)
+
+	assert.Nil(c.Mail("sender@example.org"))
+	assert.Nil(c.Rcpt("recipient@example.net"))
+
+	wc, err := c.Data()
+	assert.Nil(err)
+	_, err = fmt.Fprintf(wc, "This is the email body")
+	assert.Nil(err)
+	assert.Nil(wc.Close())
+
+	assert.Nil(c.Quit())
+
+	select {
+	case <-called:
+	case <-time.After(time.Second):
+		t.Log("timed out")
+		t.Fail()
+	}
 }
 
 func TestSendMultipleMessagesWithSameConnection(t *testing.T) {
 	assert := assert.New(t)
 
-	with(t, func(s Server) {
-		called := make(chan struct{})
-		calls := 0
-		s.Handle(func(msg Message) {
-			switch calls {
-			case 0:
-				assert.Equal("sender@example.org", msg.Sender)
-				if assert.Equal(1, len(msg.Recipients)) {
-					assert.Equal("recipient@example.net", msg.Recipients[0])
-				}
-				assert.Equal("This is the email body\n", msg.Data)
-			case 1:
-				assert.Equal("sender2@example.org", msg.Sender)
-				if assert.Equal(1, len(msg.Recipients)) {
-					assert.Equal("recipient2@example.net", msg.Recipients[0])
-				}
-				assert.Equal("This is the email body 2\n", msg.Data)
-				close(called)
-			default:
-				t.Fail()
+	s := NewServer(t)
+	defer s.Close()
+
+	called := make(chan struct{})
+	calls := 0
+	s.Handle(func(msg Message) {
+		switch calls {
+		case 0:
+			assert.Equal("sender@example.org", msg.Sender)
+			if assert.Equal(1, len(msg.Recipients)) {
+				assert.Equal("recipient@example.net", msg.Recipients[0])
 			}
-
-			calls++
-		})
-
-		c, err := smtp.Dial(ADDR)
-		assert.Nil(err)
-
-		assert.Nil(c.Mail("sender@example.org"))
-		assert.Nil(c.Rcpt("recipient@example.net"))
-
-		wc, err := c.Data()
-		assert.Nil(err)
-		_, err = fmt.Fprintf(wc, "This is the email body")
-		assert.Nil(err)
-		assert.Nil(wc.Close())
-
-		assert.Nil(c.Mail("sender2@example.org"))
-		assert.Nil(c.Rcpt("recipient2@example.net"))
-
-		wc, err = c.Data()
-		assert.Nil(err)
-		_, err = fmt.Fprintf(wc, "This is the email body 2")
-		assert.Nil(err)
-		assert.Nil(wc.Close())
-
-		assert.Nil(c.Quit())
-
-		select {
-		case <-called:
-		case <-time.After(time.Second):
-			t.Log("timed out")
+			assert.Equal("This is the email body\n", msg.Data)
+		case 1:
+			assert.Equal("sender2@example.org", msg.Sender)
+			if assert.Equal(1, len(msg.Recipients)) {
+				assert.Equal("recipient2@example.net", msg.Recipients[0])
+			}
+			assert.Equal("This is the email body 2\n", msg.Data)
+			close(called)
+		default:
 			t.Fail()
 		}
+
+		calls++
 	})
+
+	c, err := smtp.Dial(ADDR)
+	assert.Nil(err)
+
+	assert.Nil(c.Mail("sender@example.org"))
+	assert.Nil(c.Rcpt("recipient@example.net"))
+
+	wc, err := c.Data()
+	assert.Nil(err)
+	_, err = fmt.Fprintf(wc, "This is the email body")
+	assert.Nil(err)
+	assert.Nil(wc.Close())
+
+	assert.Nil(c.Mail("sender2@example.org"))
+	assert.Nil(c.Rcpt("recipient2@example.net"))
+
+	wc, err = c.Data()
+	assert.Nil(err)
+	_, err = fmt.Fprintf(wc, "This is the email body 2")
+	assert.Nil(err)
+	assert.Nil(wc.Close())
+
+	assert.Nil(c.Quit())
+
+	select {
+	case <-called:
+	case <-time.After(time.Second):
+		t.Log("timed out")
+		t.Fail()
+	}
 }
 
 func TestMessageToMultipleRecipients(t *testing.T) {
 	assert := assert.New(t)
 
-	with(t, func(s Server) {
-		called := make(chan struct{})
-		s.Handle(func(msg Message) {
-			assert.Equal("sender@example.org", msg.Sender)
-			if assert.Equal(3, len(msg.Recipients)) {
-				assert.Equal("recipient1@example.net", msg.Recipients[0])
-				assert.Equal("recipient2@example.net", msg.Recipients[1])
-				assert.Equal("recipient3@example.net", msg.Recipients[2])
-			}
-			assert.Equal("This is the email body\n", msg.Data)
-			close(called)
-		})
+	s := NewServer(t)
+	defer s.Close()
 
-		c, err := smtp.Dial(ADDR)
-		assert.Nil(err)
-
-		assert.Nil(c.Mail("sender@example.org"))
-		assert.Nil(c.Rcpt("recipient1@example.net"))
-		assert.Nil(c.Rcpt("recipient2@example.net"))
-		assert.Nil(c.Rcpt("recipient3@example.net"))
-
-		wc, err := c.Data()
-		assert.Nil(err)
-		_, err = fmt.Fprintf(wc, "This is the email body")
-		assert.Nil(err)
-		assert.Nil(wc.Close())
-
-		assert.Nil(c.Quit())
-
-		select {
-		case <-called:
-		case <-time.After(time.Second):
-			t.Log("timed out")
-			t.Fail()
+	called := make(chan struct{})
+	s.Handle(func(msg Message) {
+		assert.Equal("sender@example.org", msg.Sender)
+		if assert.Equal(3, len(msg.Recipients)) {
+			assert.Equal("recipient1@example.net", msg.Recipients[0])
+			assert.Equal("recipient2@example.net", msg.Recipients[1])
+			assert.Equal("recipient3@example.net", msg.Recipients[2])
 		}
+		assert.Equal("This is the email body\n", msg.Data)
+		close(called)
 	})
+
+	c, err := smtp.Dial(ADDR)
+	assert.Nil(err)
+
+	assert.Nil(c.Mail("sender@example.org"))
+	assert.Nil(c.Rcpt("recipient1@example.net"))
+	assert.Nil(c.Rcpt("recipient2@example.net"))
+	assert.Nil(c.Rcpt("recipient3@example.net"))
+
+	wc, err := c.Data()
+	assert.Nil(err)
+	_, err = fmt.Fprintf(wc, "This is the email body")
+	assert.Nil(err)
+	assert.Nil(wc.Close())
+
+	assert.Nil(c.Quit())
+
+	select {
+	case <-called:
+	case <-time.After(time.Second):
+		t.Log("timed out")
+		t.Fail()
+	}
 }
 
 func TestSenderRecipientBodyAndQuitWithReset(t *testing.T) {
 	assert := assert.New(t)
 
-	with(t, func(s Server) {
-		called := make(chan struct{})
-		s.Handle(func(msg Message) {
-			assert.Equal("sender2@example.org", msg.Sender)
-			if assert.Equal(1, len(msg.Recipients)) {
-				assert.Equal("recipient2@example.net", msg.Recipients[0])
-			}
-			assert.Equal("This is the email body2\n", msg.Data)
-			close(called)
-		})
+	s := NewServer(t)
+	defer s.Close()
 
-		called2 := make(chan struct{})
-		s.Handle(func(msg Message) {
-			assert.Equal("sender2@example.org", msg.Sender)
-			if assert.Equal(1, len(msg.Recipients)) {
-				assert.Equal("recipient2@example.net", msg.Recipients[0])
-			}
-			assert.Equal("This is the email body2\n", msg.Data)
-			close(called2)
-		})
-
-		c, err := smtp.Dial(ADDR)
-		assert.Nil(err)
-
-		assert.Nil(c.Mail("sender@example.org"))
-		assert.Nil(c.Rcpt("recipient@example.net"))
-
-		assert.Nil(c.Reset())
-
-		assert.Nil(c.Mail("sender2@example.org"))
-		assert.Nil(c.Rcpt("recipient2@example.net"))
-
-		wc, err := c.Data()
-		assert.Nil(err)
-		_, err = fmt.Fprintf(wc, "This is the email body2")
-		assert.Nil(err)
-		assert.Nil(wc.Close())
-
-		assert.Nil(c.Quit())
-
-		select {
-		case <-called:
-		case <-time.After(time.Second):
-			t.Log("timed out")
-			t.Fail()
+	called := make(chan struct{})
+	s.Handle(func(msg Message) {
+		assert.Equal("sender2@example.org", msg.Sender)
+		if assert.Equal(1, len(msg.Recipients)) {
+			assert.Equal("recipient2@example.net", msg.Recipients[0])
 		}
-
-		select {
-		case <-called2:
-		case <-time.After(time.Second):
-			t.Log("timed out2")
-			t.Fail()
-		}
+		assert.Equal("This is the email body2\n", msg.Data)
+		close(called)
 	})
+
+	called2 := make(chan struct{})
+	s.Handle(func(msg Message) {
+		assert.Equal("sender2@example.org", msg.Sender)
+		if assert.Equal(1, len(msg.Recipients)) {
+			assert.Equal("recipient2@example.net", msg.Recipients[0])
+		}
+		assert.Equal("This is the email body2\n", msg.Data)
+		close(called2)
+	})
+
+	c, err := smtp.Dial(ADDR)
+	assert.Nil(err)
+
+	assert.Nil(c.Mail("sender@example.org"))
+	assert.Nil(c.Rcpt("recipient@example.net"))
+
+	assert.Nil(c.Reset())
+
+	assert.Nil(c.Mail("sender2@example.org"))
+	assert.Nil(c.Rcpt("recipient2@example.net"))
+
+	wc, err := c.Data()
+	assert.Nil(err)
+	_, err = fmt.Fprintf(wc, "This is the email body2")
+	assert.Nil(err)
+	assert.Nil(wc.Close())
+
+	assert.Nil(c.Quit())
+
+	select {
+	case <-called:
+	case <-time.After(time.Second):
+		t.Log("timed out")
+		t.Fail()
+	}
+
+	select {
+	case <-called2:
+	case <-time.After(time.Second):
+		t.Log("timed out2")
+		t.Fail()
+	}
 }
 
 func TestVerify(t *testing.T) {
 	assert := assert.New(t)
 
-	with(t, func(s Server) {
-		c, err := smtp.Dial(ADDR)
-		assert.Nil(err)
+	s := NewServer(t)
+	defer s.Close()
 
-		assert.Equal(c.Verify("sender@example.org").Error(), "252 Cannot VRFY user, but will attempt delivery")
+	c, err := smtp.Dial(ADDR)
+	assert.Nil(err)
 
-		assert.Nil(c.Quit())
-	})
+	assert.Equal(c.Verify("sender@example.org").Error(), "252 Cannot VRFY user, but will attempt delivery")
+
+	assert.Nil(c.Quit())
 }
 
 func TestHelp(t *testing.T) {
-	with(t, func(s Server) {
-		c := NewClient(t)
+	s := NewServer(t)
+	defer s.Close()
 
-		c.Send("HELP")
-		assert.Equal(t, c.ReadLine(), "502 Command not implemented")
-	})
+	c := NewClient(t)
+
+	c.Send("HELP")
+	assert.Equal(t, c.ReadLine(), "502 Command not implemented")
 }
 
 func TestNoop(t *testing.T) {
-	with(t, func(s Server) {
-		c := NewClient(t)
+	s := NewServer(t)
+	defer s.Close()
 
-		c.Send("NOOP")
-		assert.Equal(t, c.ReadLine(), "250 Ok")
-	})
+	c := NewClient(t)
+
+	c.Send("NOOP")
+	assert.Equal(t, c.ReadLine(), "250 Ok")
 }
 
 func TestUnrecognizedCommand(t *testing.T) {
-	with(t, func(s Server) {
-		c := NewClient(t)
+	s := NewServer(t)
+	defer s.Close()
 
-		c.Send("LOOK")
-		assert.Equal(t, c.ReadLine(), "500 Command unrecognized")
-	})
+	c := NewClient(t)
+
+	c.Send("LOOK")
+	assert.Equal(t, c.ReadLine(), "500 Command unrecognized")
 }
 
 // HELO
